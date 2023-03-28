e5e04baccf5329a5f3debf13eae7c3943e0fa29b
evilaks <evilaks@gmail.com>
2023-03-28 17:21:37 +0400

    fix: userDto validation

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -3,11 +3,16 @@ package ru.practicum.shareit.user.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 
 @Data
 @Builder
 public class UserDto {
     private Long id;
+    @NotBlank
     private String name;
+    @Email
     private String email;
 }
