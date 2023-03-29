1f936b8f04fc97a22dc8ed8a25cd26e353c3b9a4
evilaks <evilaks@gmail.com>
2023-03-29 17:57:33 +0400

    feat: add ResponseEntity to controllers

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
@@ -17,38 +18,38 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping()
-    public ItemDto addItem(@RequestHeader(value = "X-Sharer-User-Id") Long ownerId, @RequestBody ItemDto itemDto) {
+    public ResponseEntity<ItemDto> addItem(@RequestHeader(value = "X-Sharer-User-Id") Long ownerId, @RequestBody ItemDto itemDto) {
         log.debug("Received POST request to /items endpoint with userId={} and {}", ownerId, itemDto);
-        return itemService.add(ownerId, itemDto);
+        return ResponseEntity.ok().body(itemService.add(ownerId, itemDto));
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@RequestHeader(value = "X-Sharer-User-Id") Long ownerId,
+    public ResponseEntity<ItemDto> updateItem(@RequestHeader(value = "X-Sharer-User-Id") Long ownerId,
                               @RequestBody ItemDto itemDto,
                               @PathVariable Long itemId) {
         log.debug("Received PATCH request to /items/{} endpoint with userId={} and {}", itemId, ownerId, itemDto);
-        return itemService.update(ownerId, itemId, itemDto);
+        return ResponseEntity.ok().body(itemService.update(ownerId, itemId, itemDto));
     }
 
     @GetMapping
-    public List<ItemDto> findAllItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<List<ItemDto>> findAllItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId) {
         log.debug("Received GET request to /items endpoint with userId={}", userId);
-        return itemService.findAll(userId);
+        return ResponseEntity.ok().body(itemService.findAll(userId));
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto findItemById(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
+    public ResponseEntity<ItemDto> findItemById(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
                                 @PathVariable Long itemId) {
         log.debug("Received GET request to /items/{} endpoint with userId={}", itemId, userId);
-        return itemService.findById(itemId, userId);
+        return ResponseEntity.ok().body(itemService.findById(itemId, userId));
     }
 
     @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
+    public ResponseEntity<List<ItemDto>> searchItems(@RequestHeader(value = "X-Sharer-User-Id") Long userId,
                                     @RequestParam String text) {
         log.debug("Received GET request to /search endpoint with userId={} and text-param={}", userId, text);
 
-        return itemService.search(userId, text);
+        return ResponseEntity.ok().body(itemService.search(userId, text));
     }
 
     @DeleteMapping("/{itemId}")
@@ -57,30 +58,5 @@ public class ItemController {
         log.debug("Received DELETE request to /items/{} endpoint with userId={}", itemId, userId);
         itemService.delete(userId, itemId);
     }
- }
 
-/* todo
-
-    Добавление новой вещи. Будет происходить по эндпойнту POST /items.
-    На вход поступает объект ItemDto. userId в заголовке X-Sharer-User-Id — это идентификатор пользователя,
-    который добавляет вещь. Именно этот пользователь — владелец вещи.
-    Идентификатор владельца будет поступать на вход в каждом из запросов, рассмотренных далее.
-
-    Редактирование вещи. Эндпойнт PATCH /items/{itemId}. Изменить можно название, описание и статус доступа к аренде.
-    Редактировать вещь может только её владелец.
-
-    Просмотр информации о конкретной вещи по её идентификатору. Эндпойнт GET /items/{itemId}.
-    Информацию о вещи может просмотреть любой пользователь.
-
-    Просмотр владельцем списка всех его вещей с указанием названия и описания для каждой. Эндпойнт GET /items.
-
-    Поиск вещи потенциальным арендатором. Пользователь передаёт в строке запроса текст, и система ищет вещи,
-    содержащие этот текст в названии или описании. Происходит по эндпойнту /items/search?text={text},
-    в text передаётся текст для поиска. Проверьте, что поиск возвращает только доступные для аренды вещи.
-
-    Для каждого из данных сценариев создайте соответственный метод в контроллере. Также создайте интерфейс ItemService
-    и реализующий его класс ItemServiceImpl, к которому будет обращаться ваш контроллер.
-    В качестве DAO создайте реализации, которые будут хранить данные в памяти приложения.
-    Работу с базой данных вы реализуете в следующем спринте.
-
- */
+}
--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
@@ -9,9 +10,7 @@ import ru.practicum.shareit.user.service.UserService;
 import javax.validation.Valid;
 import java.util.List;
 
-/**
- * TODO Sprint add-controllers.
- */
+
 @Slf4j
 @RestController
 @RequestMapping(path = "/users")
@@ -21,27 +20,27 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping()
-    public UserDto addUser(@RequestBody @Valid UserDto userDto) {
+    public ResponseEntity<UserDto> addUser(@RequestBody @Valid UserDto userDto) {
         log.debug("Received POST request to /users endpoint with User-object {}", userDto);
-        return userService.add(userDto);
+        return ResponseEntity.ok().body(userService.add(userDto));
     }
 
     @PatchMapping("/{userId}")
-    public UserDto updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
         log.debug("Received PATCH request to /users/{} endpoint with User-object {}", userId, userDto);
-        return userService.update(userId, userDto);
+        return ResponseEntity.ok().body(userService.update(userId, userDto));
     }
 
     @GetMapping()
-    public List<UserDto> findAllUsers() {
+    public ResponseEntity<List<UserDto>> findAllUsers() {
         log.debug("Received GET request to /users endpoint");
-        return userService.findAll();
+        return ResponseEntity.ok().body(userService.findAll());
     }
 
     @GetMapping("/{userId}")
-    public UserDto findUserById(@PathVariable Long userId) {
+    public ResponseEntity<UserDto> findUserById(@PathVariable Long userId) {
         log.debug("Received GET request to /users/{} endpoint", userId);
-        return userService.findById(userId);
+        return ResponseEntity.ok().body(userService.findById(userId));
     }
 
     @DeleteMapping("/{userId}")
@@ -49,23 +48,6 @@ public class UserController {
         log.debug("Received DELETE request to /users/{} endpoint", userId);
         userService.delete(userId);
     }
+
 }
 
-/*  todo
-
-    Создайте класс UserController и методы в нём для основных CRUD-операций.
-    Также реализуйте сохранение данных о пользователях в памяти.
-
-    Сделать интерфейс сервиса и его имплементацию.
-
-    Сделать хранение в памяти, тоже через интерфейс и его имплементацию.
-
-    Эндпоинты:
-
-    POST /users - create user
-    PATCH /users/id - update user
-    GET /users - get all users
-    GET /users/id - get user by id
-    DELETE /users/id - delete user by id
-
- */
