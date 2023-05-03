4313b397d1d945dd12bfe83271ebd1b8315e5a05
evilaks <evilaks@gmail.com>
2023-05-03 13:58:41 +0400

    feat: add dtomappers tests

--- src/main/java/ru/practicum/shareit/booking/dto/IncomingBookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/IncomingBookingDto.java
@@ -1,13 +1,17 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.model.BookingStatus;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class IncomingBookingDto {
     private Long id;
     private LocalDateTime start;
